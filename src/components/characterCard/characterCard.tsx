2d8b56c7b8ffc7a8acc3c98f80447184ef7c2128
Kaan Küreli <kkureli@gmail.com>
2024-03-20 19:07:09 +0300

    feat: keyboard navigation added

--- src/components/characterCard/characterCard.tsx
+++ src/components/characterCard/characterCard.tsx
@@ -7,49 +7,55 @@ type Props = {
   isSelected: boolean;
   onSelect: () => void;
   query: string;
+  onKeyDown: (event: React.KeyboardEvent<HTMLInputElement>) => void; // Klavye olaylarını dinlemek için
 };
 
-const CharacterCard = (props: Props) => {
-  const { character, onSelect, isSelected, query } = props;
+const CharacterCard = React.forwardRef<HTMLInputElement, Props>(
+  (props, ref) => {
+    const { character, onSelect, isSelected, query, onKeyDown } = props;
 
-  const highlightName = (name: string, query: string): React.ReactNode => {
-    const regex = new RegExp(`(${query})`, "gi");
-    const parts = name.split(regex);
+    const highlightName = (name: string, query: string): React.ReactNode => {
+      const regex = new RegExp(`(${query})`, "gi");
+      const parts = name.split(regex);
+
+      return (
+        <>
+          {parts.map((part, index) =>
+            regex.test(part) ? (
+              <strong key={index}>{part}</strong>
+            ) : (
+              <span key={index}>{part}</span>
+            )
+          )}
+        </>
+      );
+    };
 
     return (
-      <>
-        {parts.map((part, index) =>
-          regex.test(part) ? (
-            <strong key={index}>{part}</strong>
-          ) : (
-            <span key={index}>{part}</span>
-          )
-        )}
-      </>
-    );
-  };
+      <div className="character-card-container" key={character.id}>
+        <input
+          ref={ref}
+          onKeyDown={onKeyDown}
+          tabIndex={0}
+          type="checkbox"
+          className="cardCheckbox"
+          name="cardCheckbox"
+          checked={isSelected}
+          onChange={onSelect}
+        />
 
-  return (
-    <div className="character-card-container" key={character.id}>
-      <input
-        type="checkbox"
-        className="cardCheckbox"
-        name="cardCheckbox"
-        checked={isSelected}
-        onChange={onSelect}
-      />
-
-      <img
-        className="character-card-img"
-        src={character.image}
-        alt={character.name}
-      />
-      <div className="card-text-info-container">
-        <p>{highlightName(character.name, query)}</p>
-        <p>{`${character.episode.length} episodes`}</p>
+        <img
+          className="character-card-img"
+          src={character.image}
+          alt={character.name}
+        />
+        <div className="card-text-info-container">
+          <p>{highlightName(character.name, query)}</p>
+          <p>{`${character.episode.length} episodes`}</p>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default CharacterCard;
