f2fe7c48b12d23ea14d1d75611c31965ff34c37b
Kaan Küreli <kkureli@gmail.com>
2024-03-21 00:08:29 +0300

    refactor: unused codes removed

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import headerLogo from "../../assets/Rick_and_Morty.png";
 import "./header.css";
-type Props = {};
 
-const Header = (props: Props) => {
+const Header = () => {
   return (
     <div className="header">
       <img src={headerLogo} alt="header" />
--- src/components/video/video.tsx
+++ src/components/video/video.tsx
@@ -8,7 +8,6 @@ const Video = () => {
         "https://www.youtube.com/embed/TgqiSBxvdws?si=bs2_upfMX75GEQrT&autoplay=1"
       }
       width="100%"
-      //   frameborder="0"
       allow="autoplay; fullscreen; picture-in-picture"
       className="embed-video"
     />
