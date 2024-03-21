8b877cbe4014fab418754b8eeb02ef71a2ca37e3
Kaan Küreli <kkureli@gmail.com>
2024-03-21 10:15:49 +0300

    feat: stars effect added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/header/header";
+import Stars from "./components/stars/stars";
 import HomeView from "./views/home/homeView";
 
 const App = (): JSX.Element => {
   return (
     <>
+      <Stars />
       <Header />
       <HomeView />
     </>
---
+++ src/components/stars/stars.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./stars.css";
+const Stars = () => {
+  return (
+    <div className="stars-container">
+      <div className="stars" />
+    </div>
+  );
+};
+
+export default Stars;
